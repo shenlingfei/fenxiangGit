78c34ca845012b3618fc81cef4052b048499ca52
shenlingfei <shen_ling_fei@163.com>
2016-01-18 15:14:57 +0800

    first add testgit

---
+++ src/com/git/test/Hello.java
@@ -0,0 +1,28 @@
+/**
+ * Project Name:testgit
+ * File Name:Hello.java
+ * Package Name:com.git.test
+ * Date:2016-1-18обнГ2:57:18
+ * Copyright (c) 2016, chenzhou1025@126.com All Rights Reserved.
+ *
+*/
+
+package com.git.test;
+/**
+ * ClassName:Hello <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2016-1-18 обнГ2:57:18 <br/>
+ * @author   Administrator
+ * @version  
+ * @since    JDK 1.6
+ * @see 	 
+ */
+public class Hello {
+	
+	public static void main(String args[]){
+		System.out.println("hello world");
+	}
+
+}
+
